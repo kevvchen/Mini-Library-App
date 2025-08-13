5a72a9e46375e4604afa9afb956d538e0b3b5081
Kevin <k3v1nnchen@gmail.com>
2025-08-13 18:53:58 -0400

    Created files

---
+++ scripts.js
@@ -0,0 +1,12 @@
+const myLibrary = []
+
+// Book Constructor
+function Book(title, author, pages, isRead) {
+    
+}
+
+// Create a book then store it in array 
+function addBookToLibrary() {
+    
+}
+
