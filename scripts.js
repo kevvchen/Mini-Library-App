ed3c278705b3edbe77a1082bdb8bcff15c439859
Kevin <k3v1nnchen@gmail.com>
2025-08-17 00:33:11 -0400

    Created functionality to create new Book objects and add them to array

--- scripts.js
+++ scripts.js
@@ -1,12 +1,22 @@
 const myLibrary = []
 
 // Book Constructor
-function Book(title, author, pages, isRead) {
-    
+function Book(id, title, author, pages, isRead) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
 }
 
-// Create a book then store it in array 
-function addBookToLibrary() {
-    
-}
+// Creates a book then store it in the array
+function addToLibrary(id, title, author, pages, isRead) {
+    const newBook = new Book(id, title, author, pages, isRead)
+    myLibrary.push(newBook)
+}   
 
+addToLibrary(crypto.randomUUID(), "The Catcher in the Rye", "J.D. Salinger", 304, false)
+addToLibrary(crypto.randomUUID(), "Atomic Habits", "James Clear", 320, true)
+addToLibrary(crypto.randomUUID(), "Ikigai: The Japanese Secret to a Long and Happy Life", "Hector Garcia, Francesc Miralles", 208, true)
+
+console.log(myLibrary)
