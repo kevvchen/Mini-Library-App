aae9a291e90f3ca44e1c81b485474dafba99139b
Kevin <k3v1nnchen@gmail.com>
2025-08-19 01:28:03 -0400

    Grid Layout for book section created

--- scripts.js
+++ scripts.js
@@ -1,26 +1,75 @@
-const myLibrary = []
+const myLibrary = [];
 
 // Book Constructor
 function Book(id, title, author, pages, isRead) {
-    this.id = id;
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.isRead = isRead;
+  this.id = id;
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
 }
 
 // Creates a book then store it in the array
 function addToLibrary(id, title, author, pages, isRead) {
-    const newBook = new Book(id, title, author, pages, isRead)
-    myLibrary.push(newBook)
-}   
-
-function displayBooks() {
-    
+  const newBook = new Book(id, title, author, pages, isRead);
+  myLibrary.push(newBook);
 }
 
-addToLibrary(crypto.randomUUID(), "The Catcher in the Rye", "J.D. Salinger", 304, false)
-addToLibrary(crypto.randomUUID(), "Atomic Habits", "James Clear", 320, true)
-addToLibrary(crypto.randomUUID(), "Ikigai: The Japanese Secret to a Long and Happy Life", "Hector Garcia, Francesc Miralles", 208, true)
+// Populate array with a couple of books so far
+function initialTests() {
+  addToLibrary(
+    crypto.randomUUID(),
+    "The Catcher in the Rye",
+    "J.D. Salinger",
+    304,
+    false
+  );
+  addToLibrary(crypto.randomUUID(), "Atomic Habits", "James Clear", 320, true);
+  addToLibrary(
+    crypto.randomUUID(),
+    "Ikigai: The Japanese Secret to a Long and Happy Life",
+    "Hector Garcia, Francesc Miralles",
+    208,
+    true
+  );
 
-console.log(myLibrary)
+  displayBooks();
+}
+
+function displayBooks() {
+  const bookContainer = document.querySelector(".books-section");
+
+  // Go through each array and create DOM elements
+  myLibrary.forEach((book) => {
+    // Create Card Container
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("book-card");
+
+    // Create book title
+    const bookTitle = document.createElement("h3");
+    bookTitle.textContent = `${book.title}`;
+    bookCard.appendChild(bookTitle);
+
+    // Create book author
+    const bookAuthor = document.createElement("h6");
+    bookAuthor.textContent = `By ${book.author}`;
+    bookCard.appendChild(bookAuthor);
+
+    // Create book pages
+    const bookPages = document.createElement("p");
+    bookPages.textContent = `${book.pages} pages`;
+    bookCard.appendChild(bookPages);
+
+    // Create book buttons (isRead toggle button, delete book button)
+    const bookButtons = document.createElement("div");
+    bookButtons.classList.add("book-buttons");
+    const isReadButton = document.createElement("button");
+    isReadButton.textContent = `${book.isRead ? "Read" : "Not Read"}`;
+    bookButtons.appendChild(isReadButton);
+    bookCard.appendChild(bookButtons);
+
+    bookContainer.appendChild(bookCard);
+  });
+}
+
+initialTests();
